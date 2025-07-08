03c8d9d7e51b82213c85efb85c60dbba71832f22
Hakyur <drogotovsky@gmail.com>
2025-07-09 02:11:19 +0300

    init commit

---
+++ src/main/java/ru/rogotovskiy/neoflexbookapi/NeoflexBookApiApplication.java
@@ -0,0 +1,13 @@
+package ru.rogotovskiy.neoflexbookapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoflexBookApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeoflexBookApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/rogotovskiy/neoflexbookapi/NeoflexBookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rogotovskiy.neoflexbookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoflexBookApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
